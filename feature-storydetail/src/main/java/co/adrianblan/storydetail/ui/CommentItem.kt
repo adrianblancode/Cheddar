cb2ecdfa332a151cc4407bbc93ee0cb479e416f5
Adrian Blanco <adrianblp@gmail.com>
2020-05-02 18:57:22 +0200

    Visual improvements to comments view

--- feature-storydetail/src/main/java/co/adrianblan/storydetail/ui/CommentItem.kt
+++ feature-storydetail/src/main/java/co/adrianblan/storydetail/ui/CommentItem.kt
@@ -1,6 +1,9 @@
 package co.adrianblan.storydetail.ui
 
 import android.text.Html
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.AbsoluteSizeSpan
 import androidx.compose.Composable
 import androidx.ui.core.DensityAmbient
 import androidx.ui.core.Modifier
@@ -15,11 +18,15 @@ import androidx.ui.layout.*
 import androidx.ui.material.MaterialTheme
 import androidx.ui.res.colorResource
 import androidx.ui.res.stringResource
+import androidx.ui.text.AnnotatedString
+import androidx.ui.text.SpanStyle
 import androidx.ui.unit.Px
+import androidx.ui.unit.TextUnit
 import androidx.ui.unit.dp
 import androidx.ui.unit.times
 import co.adrianblan.storydetail.FlatComment
 import co.adrianblan.storydetail.R
+
 
 @Composable
 fun CommentItem(comment: FlatComment, storyAuthor: String?) {
@@ -39,10 +46,10 @@ fun CommentItem(
     storyAuthor: String?
 ) {
 
-    val depthIndicatorWidth = 12.dp
+    val depthIndicatorWidth = 10.dp
 
     val strokeWidthPx: Px = with(DensityAmbient.current) {
-        1.2f.dp.toPx()
+        1f.dp.toPx()
     }
 
     val depthIndicatorPaint = Paint().apply {
@@ -55,7 +62,7 @@ fun CommentItem(
         paddingStart = 16.dp + depthIndex * depthIndicatorWidth,
         paddingEnd = 16.dp,
         paddingTop = 8.dp,
-        paddingBottom = 6.dp,
+        paddingBottom = 8.dp,
         modifier = Modifier.fillMaxWidth() +
                 Modifier.drawBehind {
 
@@ -63,10 +70,10 @@ fun CommentItem(
                     var depthOffset: Px = 16.dp.toPx()
 
                     repeat(depthIndex) {
-                        val o1 = Offset(depthOffset.value, 4.dp.toPx().value)
+                        val o1 = Offset(depthOffset.value, 0f)
                         val o2 = Offset(
                             depthOffset.value,
-                            parentSize.height.value - 4.dp.toPx().value
+                            parentSize.height.value
                         )
 
                         drawLine(o1, o2, depthIndicatorPaint)
@@ -83,12 +90,12 @@ fun CommentItem(
             val isDeleted = by == null
 
             if (isDeleted) {
-                Spacer(Modifier.preferredHeight(12.dp))
+                Spacer(Modifier.preferredHeight(8.dp))
                 Text(
                     text = stringResource(R.string.comment_deleted_title),
                     style = MaterialTheme.typography.subtitle2
                 )
-                Spacer(Modifier.preferredHeight(16.dp))
+                Spacer(Modifier.preferredHeight(12.dp))
             } else {
 
                 val isStoryAuthor = by == storyAuthor
@@ -105,12 +112,30 @@ fun CommentItem(
                     text = by.orEmpty() + authorSuffix,
                     style = MaterialTheme.typography.subtitle2.copy(color = authorColor)
                 )
-                Spacer(Modifier.preferredHeight(2.dp))
+                Spacer(Modifier.preferredHeight(4.dp))
                 Text(
-                    text = Html.fromHtml(text.orEmpty()).toString().trimEnd(),
+                    text = Html.fromHtml(text.orEmpty()).toString().trimEnd()
+                        .reduceParagraphSpacing(),
                     style = MaterialTheme.typography.body2
                 )
             }
         }
     }
+}
+
+// Comments have a lot of whitespace between paragraphs, let's reduce it
+private fun String.reduceParagraphSpacing(): AnnotatedString {
+    val builder = AnnotatedString.Builder(this)
+
+    Regex.fromLiteral("\n\n")
+        .findAll(this)
+        .forEach { match ->
+            builder.addStyle(
+                SpanStyle(fontSize = TextUnit.Sp(6)),
+                match.range.first,
+                match.range.last + 1
+            )
+        }
+
+    return builder.toAnnotatedString()
 }
